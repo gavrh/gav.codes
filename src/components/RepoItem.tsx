37b994e679efe7d32eaf15c201ac304deedc4074
gavin <96849112+g-sd@users.noreply.github.com>
2024-11-08 19:53:48 -0800

    config, context, components

---
+++ src/components/RepoItem.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function RepoItem () {
+    return (
+        <div>
+            github repo
+        </div>
+    )
+}
+
+// RepoItem styled components
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-
+import { BrowserRouter as Router, Switch as Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <App />,
+  <div className='bg-[#121212] overlfow-y-scroll flex justify-center'>
+    <Router basename='/'>      
+      <Routes>
+        <Route path='/:page?' render={App} />
+      </Routes>
+    </Router>
+  </div>,
   document.getElementById('root')
 );
