6686dc02fe9880a20d2977203c1bf0e8f1879d00
Gavin Holmes <gavin@gav.codes>
2025-04-07 17:50:31 -0700

    working start to webpack react, 0 dependecy vulns

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+).render(
+    <div>
+        <Router basename="/">
+            <Routes>
+                <Route path="*" element={<App />} />
+            </Routes>
+        </Router>
+    </div>
+)
