761a6ac777e205f2622d3d01c280ddcfdf5674dd
Gavin Holmes <gavin@gav.codes>
2025-05-13 07:16:41 -0700

    setup constants and store refresh

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"gav.codes/site/handlers"
+	"gav.codes/site/store"
 	"gav.codes/site/templates"
+
+	"sync/atomic"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -10,13 +13,71 @@ import (
 
 func main() {
 
+	consts := store.Constants {
+		Name: "gavin holmes",
+		Age: 19,
+		City: "San Francisco",
+		State: "CA",
+		Socials: [][]string {
+			{
+				"resume",
+				"https://gav.codes/static/assets/Gavin_Holmes_Resume.pdf",
+			}, {
+				"github",
+				"https://github.com/gavrh",
+			}, {
+				"linkedin",
+				"https://linkedin.com/in/gavrh",
+			}, {
+				"email",
+				"mailto:gavin@gav.codes",
+			},
+		},
+		Experience: [][]string {
+			{
+				"systems software intern",
+				"subtrace",
+				"https://subtrace.dev",
+				"jun 2025",
+				"present",
+			}, {
+				"computer science tutor",
+				"freelance",
+				"",
+				"apr 2023",
+				"present",
+			}, {
+				"freelance swe",
+				"fiverr",
+				"https://fiverr.com",
+				"jan 2021",
+				"apr 2025",
+			},
+		},
+	}
+
+	var atom atomic.Value
+	atom.Store(store.Store { Repos: []store.Repo{} })
+	go store.RefreshStore(
+		&atom,
+		"gavrh",
+		[]string {
+			"conve",
+			"germ",
+			"spotless",
+			"librespot-c",
+			"scrapbook",
+			"gav.codes",
+		},
+	)
+
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.IPExtractor = echo.ExtractIPDirect()
 	e.Static("/static/assets", "assets")
 	e.Static("/static/css", "css")
 	e.Renderer = templates.NewTemplate()
-	handlers.HandleRequests(e)
+	handlers.HandleRequests(e, &consts, &atom)
 	e.Start(":6969")
 
 }
